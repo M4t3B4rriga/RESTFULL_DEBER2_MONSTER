8b58104b9e30ffb869eec614e578a8bfdc328d08
Adali Carvajal <77343598+adalicarvajal@users.noreply.github.com>
2025-05-16 18:28:22 -0500

    Add files via upload

---
+++ 01. SERVIDOR/WSConversionUnidades/WSConversionUnidades/ec.edu.monster.controlador/RESTLoginControllerUnitConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSConversionUnidades.ec.edu.monster.modelo;
+using WSConversionUnidades.ec.edu.monster.modelo;
+
+
+
+
+namespace WSConversionUnidades.ec.edu.monster.controlador
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RESTLoginControllerUnitConverter : ControllerBase
+    {
+        [HttpPost("Login")]
+        public ActionResult<bool> Login([FromBody] UserModel user)
+        {
+            if (user.UserName == "MONSTER" && user.Password == "MONSTER9")
+            {
+                return Ok(true);
+            }
+            return BadRequest(false);
+        }
+    }
+}
---
+++ 01. SERVIDOR/WSConversionUnidades/WSConversionUnidades/ec.edu.monster.controlador/RESTUnitConverter.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WSConversionUnidades.ec.edu.monster.controlador
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RESTUnitConverter : ControllerBase
+    {
+        [HttpPost("KgToLib")]
+        public ActionResult<double> KgToLb([FromBody] string kg)
+        {
+            if (!double.TryParse(kg, out var value))
+                return BadRequest("Valor inválido");
+
+            return Ok(value * 2.20462);
+        }
+
+        [HttpPost("LbToKg")]
+        public ActionResult<double> LbToKg([FromBody] string lb)
+        {
+            if (!double.TryParse(lb, out var value))
+                return BadRequest("Valor inválido");
+
+            return Ok(value / 2.20462);
+        }
+
+        [HttpPost("CmToM")]
+        public ActionResult<double> CmToM([FromBody] string cm)
+        {
+            if (!double.TryParse(cm, out var value))
+                return BadRequest("Valor inválido");
+
+            return Ok(value / 100.0);
+        }
+
+        [HttpPost("MToKm")]
+        public ActionResult<double> MToKm([FromBody] string m)
+        {
+            if (!double.TryParse(m, out var value))
+                return BadRequest("Valor inválido");
+
+            return Ok(value / 1000.0);
+        }
+
+        [HttpPost("KmToM")]
+        public ActionResult<double> KmToM([FromBody] string km)
+        {
+            if (!double.TryParse(km, out var value))
+                return BadRequest("Valor inválido");
+
+            return Ok(value * 1000.0);
+        }
+
+        [HttpPost("MmToM")]
+        public ActionResult<double> MmToM([FromBody] string mm)
+        {
+            if (!double.TryParse(mm, out var value))
+                return BadRequest("Valor inválido");
+
+            return Ok(value / 1000.0);
+        }
+
+        [HttpPost("MToCm")]
+        public ActionResult<double> MToCm([FromBody] string m)
+        {
+            if (!double.TryParse(m, out var value))
+                return BadRequest("Valor inválido");
+
+            return Ok(value * 100.0);
+        }
+    }
+}
